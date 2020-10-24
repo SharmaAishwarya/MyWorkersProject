34f5f4011d01d13d0ab549ec213598109350a4bd
Aishwarya Sharma <aishwarya-sharma@uiowa.edu>
2020-10-24 14:24:49 -0500

    the links endpoint is working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 addEventListener('fetch', event => {
+  const { request } = event
   event.respondWith(handleRequest(event.request))
 })
 /**
@@ -6,7 +7,23 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+
+  if(request.method == "GET" && request.url.match('json-api.aishwaryasharma8961.workers.dev/links')){
+    return new Response(JSON.stringify(links),{
+      headers:{'content-type': 'application/json'}
+    })
+  } else {
+    return new Response('Hello worker!', {
+      headers: { 'content-type': 'text/plain' },
+    })
+  }
+  
 }
+
+var links =[{name: "amazon" ,
+link:"https://www.amazon.com/"},
+{name: "google" ,
+            link:"https://www.google.com/"},
+{name: "cloudflare" ,
+            link:"https://developers.cloudflare.com/workers/learning/getting-started#4b-routing-and-filtering-requests"}
+]
